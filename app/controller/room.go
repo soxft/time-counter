ac909e8628011145bd9225922972af262bef8e1a
Zkeq <admin@icodeq.com>
2023-02-06 13:14:57 +0800

    chore(room): Removed some comments

--- app/controller/room.go
+++ app/controller/room.go
@@ -14,10 +14,6 @@ import (
 var RawFile []byte
 
 func Room(c *gin.Context) {
-	// c.JSON(200,gin.H{})
-	// c.HTML(http.StatusOK, "dist/index2.html", gin.H{
-	// 	"room": "room1",
-	// })
 
 	rawHtml := strings.ReplaceAll(string(RawFile), "{{.room}}", c.Param("room"))
 	c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(rawHtml))
